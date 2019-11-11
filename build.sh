6f324547c00733ccc3699386bd1fcd303371f37e
nokdupark <nokdupark@gmail.com>
2019-11-12 02:19:30 +0900

    tf2-nqa

---
+++ build.sh
@@ -0,0 +1,1 @@
+docker build -f Dockerfile-jupyter --no-cache=True -t nokdupark/tf-v2-qa:latest .
---
+++ run.sh
@@ -0,0 +1,1 @@
+docker run --runtime=nvidia -it --rm -v $(pwd):/root/ tensorflow/tensorflow:2.0.0a0-gpu-py3
